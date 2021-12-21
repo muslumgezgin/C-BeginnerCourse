7ccd65debcdc7f1dd063526b039404c39aba9f1c
muslum <muslum_gezgin@hotmail.com>
2021-12-21 15:56:09 +0100

    Add Person to understand constructor

--- ClassStarter/Program.cs
+++ ClassStarter/Program.cs
@@ -1,3 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace ClassStarter;
 
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Person murat = new Person()
+        // here we call the empty constructor
+        {
+            Age = 12,
+            Name = "Murat",
+            PartnerName = "Hatun"
+        };
+
+        Person muslum = new Person("Muslum", 30, "hatun");
+        // here we call the second constructor that takes
+        // name age and partnername
+
+        // muslum.Name = "Muslum2";
+        //I can not set variable of name after initializer object 
+
+    }
+
+
+    class Person
+    {
+        public string? Name { get; init; }
+
+        public int? Age { get; set; }
+
+        public string? PartnerName { get; set; }
+
+        public Person()
+        {
+
+        }
+
+        public Person(string name, int age, string partnername)
+        {
+            Name = name;
+            Age = age;
+            PartnerName = partnername;
+
+        }
+
+
+
+    }
+
+}
